1e1b29d79bd2fdeb07c8697cf28173c5a1fa538c
Alberto <alberto.ognibene@gmail.com>
2015-10-16 23:15:39 +0200

    Exercise 3 finished.

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,5 +1,18 @@
-var compute = function() {
+var compute = function(strand1, strand2) {
+    if (strand1.length != strand2.length) {
+        throw new Error('DNA strands must be of equal length.');
+    }
 
+    var hammingDistance = 0;
+    var strandsLength = strand1.length;
+    
+    for (var i = 0; i < strandsLength; ++i) {
+        if (strand1[i] != strand2[i]) {
+            ++hammingDistance;
+        }
+    }
+
+    return hammingDistance;
 };
 
 module.exports = compute;
