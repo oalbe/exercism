468b4dc4cfdd8b93a594bda77246cec55e49963f
Alberto <alberto.ognibene@gmail.com>
2015-10-17 00:09:42 +0200

    Exercism 5 completed

---
+++ javascript/bob/bob.js
@@ -0,0 +1,23 @@
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+    if (!input || !input.trim() || 0 === input.length) {
+        return 'Fine. Be that way!';
+    }
+
+    // the regex checks if there's at least a letter in the string, either uppercase or not
+    if (/[a-zA-Z]/.test(input)) {
+        if (input === input.toUpperCase()) {
+            return 'Whoa, chill out!';
+        }
+    }
+
+    if (input[input.length - 1] == '?') {
+        return 'Sure.';
+    }
+
+    // in all the other cases:
+    return 'Whatever.';
+};
+
+module.exports = Bob;
