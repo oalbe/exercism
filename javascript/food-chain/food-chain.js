d9c5af2ddd216dfd795b16501a8645dc06c5acab
Alberto <alberto.ognibene@gmail.com>
2015-10-22 16:18:34 +0200

    Exercism food-chain refactored.

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,60 +1,38 @@
 exports.verse = verse;
 exports.verses = verses;
-// exports.song = verse.bind(null, 1, 8);
 
-function song() {
-    return {
-        verse : function(verseNumber) {
-            return verse(verseNumber);
-        }
-    };
+function verse_helper(thing, things) {
+    // 0 = fly | 7 = horse
+    if ((0 === thing) || (7 === thing)) return;
+
+    // 2 = bird
+    if (2 === thing) {
+        return 'She swallowed the ' + things[thing] + ' to catch the ' + 
+            things[thing - 1] + ' that wriggled and jiggled and tickled inside her.\n';
+    }
+
+    return 'She swallowed the ' + things[thing] + ' to catch the ' + things[thing - 1] + '.\n';
 }
 
 function verse(verseNumber) {
     var swallowedThings = ['fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow', 'horse'];
 
-    var swallowedEvents = { // ew...
-        'fly' : [
-            'I don\'t know why she swallowed the fly. ',
-            'Perhaps she\'ll die.\n'
-        ],
-        'spider' : [
-            'It wriggled and jiggled and tickled inside her.\n',
-            'She swallowed the spider to catch the fly.\n'
-        ],
-        'bird' : [
-            'How absurd to swallow a bird!\n',
-            'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n'
-        ],
-        'cat' : [
-            'Imagine that, to swallow a cat!\n',
-            'She swallowed the cat to catch the bird.\n'
-        ],
-        'dog' : [
-            'What a hog, to swallow a dog!\n',
-            'She swallowed the dog to catch the cat.\n'
-        ],
-        'goat' : [
-            'Just opened her throat and swallowed a goat!\n',
-            'She swallowed the goat to catch the dog.\n'
-        ],
-        'cow' : [
-            'I don\'t know how she swallowed a cow!\n',
-            'She swallowed the cow to catch the goat.\n'
-        ],
-        'horse' : [
-            'She\'s dead, ',
-            'of course!\n'
-        ]
-    };
-
     var beginning = 'I know an old lady who swallowed a ';
 
-    var commonComposition = 
-        beginning + 
+    var swallowedEvents = { // ew...
+        'fly' : 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n',
+        'spider' : 'It wriggled and jiggled and tickled inside her.\n',
+        'bird' : 'How absurd to swallow a bird!\n',
+        'cat' : 'Imagine that, to swallow a cat!\n',
+        'dog' : 'What a hog, to swallow a dog!\n',
+        'goat' : 'Just opened her throat and swallowed a goat!\n',
+        'cow' : 'I don\'t know how she swallowed a cow!\n',
+        'horse' : 'She\'s dead, of course!\n'
+    };
+
+    var commonComposition = beginning + 
         swallowedThings[verseNumber - 1] + '.\n' +
-        swallowedEvents[swallowedThings[verseNumber - 1]][0] +
-        swallowedEvents[swallowedThings[verseNumber - 1]][1];
+        swallowedEvents[swallowedThings[verseNumber - 1]];
 
     if ((1 === verseNumber) || (8 === verseNumber)) {
         return commonComposition;
@@ -62,13 +40,14 @@ function verse(verseNumber) {
 
     var specificComposition = '';
     for (var i = 2; i <= verseNumber; ++i) {
-        var seIndex = swallowedThings[verseNumber - i];
+        var stIndex = verseNumber - i;
+        var seIndex = swallowedThings[stIndex];
+
+        specificComposition += verse_helper(stIndex + 1, swallowedThings);
 
         if (i === verseNumber) {
-            specificComposition += swallowedEvents[seIndex][0];
+            specificComposition += swallowedEvents[seIndex];
         }
-
-        specificComposition += swallowedEvents[seIndex][1];
     }
 
     return commonComposition + specificComposition;
