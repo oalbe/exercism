1163d60d1b05bc18ddd2061619c0b0f0f0b770c0
Alberto <alberto.ognibene@gmail.com>
2015-11-02 18:55:28 +0100

    Organized /hello-worl folder, deleted duplicate files, made the new test.spec file work, and the like.

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,15 +1,11 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
-// expected here.
-
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    if (input) {
+        return ('Hello, ' + input + '!');
+    }
+
+    return 'Hello, World!';
 };
 
 module.exports = HelloWorld;
