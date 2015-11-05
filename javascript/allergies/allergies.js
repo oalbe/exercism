6ad2f85d9a20c3f8954ed065527200bca4c2faf1
Alberto <alberto.ognibene@gmail.com>
2015-11-05 15:47:28 +0100

    Exercism 25 done. It must be optimized though.

---
+++ javascript/allergies/allergies.js
@@ -0,0 +1,38 @@
+var Allergies = function(score) {
+	this.allergens = {
+		'cats': 128,
+		'pollen': 64,
+		'chocolate': 32,
+		'tomatoes': 16,
+		'strawberries': 8,
+		'shellfish': 4,
+		'peanuts': 2,
+	    'eggs': 1
+	};
+	
+	this.allergyScore = (score < 256) ? score : score % 256;
+};
+
+Allergies.prototype.list = function() {
+	var allergensList = [];
+	
+	var mScore = this.allergyScore;
+	for (var aKey in this.allergens) {
+		if (this.allergens[aKey] <= mScore) {
+			allergensList.push(aKey);
+			mScore -= this.allergens[aKey];
+		}
+	}
+	
+	// TODO: this must be optimized
+	return allergensList.reverse();
+};
+
+Allergies.prototype.allergicTo = function(allergen) {
+	var allergicToList = this.list();
+	
+	return (-1 !== allergicToList.indexOf(allergen));
+};
+
+
+module.exports = Allergies;
