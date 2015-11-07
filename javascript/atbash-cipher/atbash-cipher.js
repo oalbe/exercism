3fe05376ca86e3b60f0002b6a07b0e4df0345c48
Alberto <alberto.ognibene@gmail.com>
2015-11-07 13:21:25 +0100

    atbash-cipher.js Modified if conditions inside the loop. Now they use the square-brackets operator and compare directly with chars, instead of calling charCodeAt() and comparing with ascii.

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -4,7 +4,8 @@ var Cipher = function() {};
 // 'a' = 97
 // 'z' = 122
 Cipher.prototype.cleanInput = function(text) {
-	return text.toLowerCase().replace(/[,\.]/g, '').replace(/\s+/g, '');
+	// TODO: I have a feeling `replace()` hides a loop. Remove it.
+	return text.toLowerCase().replace(/[,\.\s+]/g, '');
 };
 
 Cipher.prototype.encode = function(text) {
@@ -13,21 +14,21 @@ Cipher.prototype.encode = function(text) {
 	var encodedText = '';
 	var cTextLength = cText.length;
 	for (var i = 0; i < cTextLength; ++i) {
-		if (cText.charCodeAt(i) >= 97 && cText.charCodeAt(i) <= 122) {
+		// if (cText.charCodeAt(i) >= 97 && cText.charCodeAt(i) <= 122) {
+		if (cText[i] >= 'a' && cText[i] <= 'z') { // Is there a cast hidden here?
 			var posEncodedChar = cText.charCodeAt(i) - 97;
 			encodedText += String.fromCharCode(122 - posEncodedChar);
-			
-		} else if (cText.charCodeAt(i) >= 48 && cText.charCodeAt(i) <= 58) {
+		} else if (cText[i] >= '0' && cText[i] <= '9') {
 			encodedText += cText[i];
 		}
 		
 		// Adds a space separator every 5 encoded characters
-		if (((i + 1) % 5) === 0) {
+		if ((i % 5) === 4) { // 4 % 5 = 4; 9 % 5 = 4; 14 % 5 = 4;
 			encodedText += ' ';
 		}
 	}
 	
-	
+	// TODO: make this trimming unnecessary.
 	return encodedText.trim();	
 };
 
