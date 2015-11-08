0b69fe7a0a3172ff8c4912c6c91ee4a1727567ba
Alberto <alberto.ognibene@gmail.com>
2015-11-08 11:33:05 +0100

    I have no idea what happened since the last commit, so I'm making a new one because the repo is in a good state at the moment.

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -1,8 +1,7 @@
 var Cipher = function() {};
 
-// space = 32
-// 'a' = 97
-// 'z' = 122
+// 'a' = ASCII 97
+// 'z' = ASCII 122
 Cipher.prototype.cleanInput = function(text) {
 	// TODO: I have a feeling `replace()` hides a loop. Remove it.
 	return text.toLowerCase().replace(/[,\.\s+]/g, '');
@@ -10,25 +9,24 @@ Cipher.prototype.cleanInput = function(text) {
 
 Cipher.prototype.encode = function(text) {
 	var cText = this.cleanInput(text);
-	
 	var encodedText = '';
+	
 	var cTextLength = cText.length;
 	for (var i = 0; i < cTextLength; ++i) {
-		// if (cText.charCodeAt(i) >= 97 && cText.charCodeAt(i) <= 122) {
 		if (cText[i] >= 'a' && cText[i] <= 'z') { // Is there a cast hidden here?
-			var posEncodedChar = cText.charCodeAt(i) - 97;
-			encodedText += String.fromCharCode(122 - posEncodedChar);
+			// cText.charCodeAt(i) - 97: position of the equivalent char, counting from z backwards
+			encodedText += String.fromCharCode(122 - (cText.charCodeAt(i) - 97));
 		} else if (cText[i] >= '0' && cText[i] <= '9') {
 			encodedText += cText[i];
 		}
 		
 		// Adds a space separator every 5 encoded characters
-		if ((i % 5) === 4) { // 4 % 5 = 4; 9 % 5 = 4; 14 % 5 = 4;
+		if ((i % 5) === 4) {
 			encodedText += ' ';
 		}
 	}
 	
-	// TODO: make this trimming unnecessary.
+	// TODO: try making this trimming unnecessary.
 	return encodedText.trim();	
 };
 
