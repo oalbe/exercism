5dbafebbb92e9ef8ddada0bce3b5f65170bdffab
Alberto <alberto.ognibene@gmail.com>
2015-11-29 15:36:39 +0100

    (javascript) pythagorean-triplet.js - Some refactoring done, adjusted the global structure of the module. Still needs some optimization on the main loop for generating triplets.

--- javascript/pythagorean-triplet/pythagorean-triplet.js
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -13,11 +13,11 @@ Triplet.prototype.product = function() {
 };
 
 function pythagorean(a, b, c) {
-	var firstSquared = Math.pow(a, 2);
-	var secondSquared = Math.pow(b, 2);
-	var thirdSquared = Math.pow(c, 2);
+	var a_sqr = Math.pow(a, 2);
+	var b_sqr = Math.pow(b, 2);
+	var c_sqr = Math.pow(c, 2);
 	
-	if ((firstSquared + secondSquared) === thirdSquared) {
+	if ((a_sqr + b_sqr) === c_sqr) {
 		return true;
 	}
 	
@@ -28,44 +28,55 @@ Triplet.prototype.isPythagorean = function() {
 	return pythagorean(this.first, this.second, this.third);
 };
 
+function tripletsGenerator(begin, end) {
+	var triplets = [];
+	
+	// Generates an array of ALL possible triplets
+	// TODO: Optimize this ugly thing. Seriously.
+	for (var a = begin; a < end; ++a) {
+		for (var b = a; b < end; ++b) {
+			for (var c = b; c < end; ++c) {
+				if (pythagorean(a, b, c)) {
+					triplets.push(new Triplet(a, b, c));
+				}
+			}
+		}
+	}
+	
+	return triplets;
+}
+
 Triplet.where = function(object) {
 	if (!object.hasOwnProperty('maxFactor')) {
 		return [];
 	}
-	
-	var generatedTriples = [];
-	
-	var triple = new Triplet(1, 1, 1);
 	
 	var startingPoint = 2;
 	if (object.hasOwnProperty('minFactor')) {
 		startingPoint = object.minFactor;
 	}
 	
-	var sumFilter = false;
+	var generatedTriplets = tripletsGenerator(startingPoint, object.maxFactor + 1);
+
+	// Filters triplets on sum if needed
 	if (object.hasOwnProperty('sum')) {
-		sumFilter = object.sum;
+		return tripletsFilter(generatedTriplets, object.sum);
 	}
 	
-	// TODO: Optimize this ugly thing. Seriously.
-	var limit = object.maxFactor + 1;
-	for (var a = startingPoint; a < limit; ++a) {
-		for (var b = a; b < limit; ++b) {
-			for (var c = b; c < limit; ++c) {
-				if (pythagorean(a, b, c)) {
-					if (sumFilter) {
-						if ((a + b + c) === sumFilter) {
-							generatedTriples.push(new Triplet(a, b, c));
-						}
-					} else {
-						generatedTriples.push(new Triplet(a, b, c));
-					}
-				}
-			}
+	return generatedTriplets;
+};
+
+function tripletsFilter(triplets, filter) {
+	var filteredTriplets = [];
+	
+	var tripletsLength = triplets.length;
+	for (var i = 0; i < tripletsLength; ++i) {
+		if (triplets[i].sum() === filter) {
+			filteredTriplets.push(triplets[i]);
 		}
 	}
-
-	return generatedTriples;
-};
+	
+	return filteredTriplets;
+}
 
 module.exports = Triplet;
