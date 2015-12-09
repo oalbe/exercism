5129a0a7077c5e360baf3c94195c90e13a550152
Alberto <alberto.ognibene@gmail.com>
2015-12-09 15:56:32 +0100

    (javascript) wordy.js - It doesn't work, but I commit anyway.

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -7,14 +7,29 @@ function WordProblem(problem) {
 	this.question = problem;
 }
 
+WordProblem.prototype.parseCommand = function() {
+
+};
+
+WordProblem.prototype.operation = function(op, lopr, ropr) {
+	switch (op) {
+		case 'plus':
+			return lopr + ropr;
+		case 'minus':
+		case 'multiplied':
+		case 'divided':
+	}
+};
+
 WordProblem.prototype.answer = function() {
-	var arr = this.question.split(' ');
+	var cleaned = this.question.replace(/\?/, '');
+	var arr = cleaned.split(' ');
 	
 	console.log(arr);
-	var result = [];
+	var result = 0;
 	for (var i = 2; i < arr.length; ++i) {
-		
+		result = this.operation(parseInt(arr[i]), arr[i + 1], parseInt(arr[i + 2]));
 	}
 	
-	return 0;
+	return result;
 };
