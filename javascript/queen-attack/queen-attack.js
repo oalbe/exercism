ae4117add5f098ac7674d5689bd2ea8539d0f714
Alberto <alberto.ognibene@gmail.com>
2015-12-29 11:05:48 +0100

    (javascript) queen-attack.js - First four commit passing.

---
+++ javascript/queen-attack/queen-attack.js
@@ -0,0 +1,42 @@
+function Queens(posObj) {
+	if ('undefined' === typeof posObj) {
+		this.white = [0, 3];
+		this.black = [7, 3];
+	} else {
+		if (posObj.white === posObj.black) {
+			throw 'Queens cannot share the same space';
+		} else {
+			this.white = posObj.white;
+			this.black = posObj.black;
+		}
+	}
+}
+
+function initMatrix() {
+	var matrix = [];
+	
+	for (var i = 0; i < 8; ++i) {
+		matrix.push([]);
+		for (var j = 0; j < 8; ++j) {
+			matrix[i].push('_');
+		}
+	}
+	
+	return matrix;
+}
+
+Queens.prototype.toString = function() {
+	var strBoard = '';
+	var matrixBoard = initMatrix();
+	
+	matrixBoard[this.white[0]][this.white[1]] = 'W';
+	matrixBoard[this.black[0]][this.black[1]] = 'B';
+	
+	for (var i = 0; i < 8; ++i) {
+		strBoard += matrixBoard[i].join(' ') + '\n';
+	}
+	
+	return strBoard;
+};
+
+module.exports = Queens;
