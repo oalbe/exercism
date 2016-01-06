4d92f68840809e6e621755294e9a8dd4bcb03cfc
Alberto <alberto.ognibene@gmail.com>
2016-01-06 13:12:05 +0100

    (python) pangram.py - Done.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -4,13 +4,10 @@ def is_pangram(input_string):
 	if ('' == input_string):
 		return False
 	
-	clean_string = input_string.lower()
-	clear_string = re.sub("[-!#$%^&*()_+|~=`{}\[\]:\";'<>?,.\/\s]*", "", clean_string)
-	print("clean_string = " + clean_string)
+	clean_string = re.sub(r"[-!#$%^&*()_+|~=`{}\[\]:\";'<>?,.\/\s]*", "", input_string.lower())
 	
 	for i in range(97, 123):
-		if (-1 == clean_string.index(chr(i))):
-			print("CHR(i) = " + chr(i))
+		if (-1 == clean_string.find(chr(i))):
 			return False
 	
 	return True
