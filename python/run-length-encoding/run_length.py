ab0616baa7890ea553465c23f92d92443657f22c
Alberto <alberto.ognibene@gmail.com>
2016-01-07 15:50:21 +0100

    (python) run_length.py - Done. Going to refactor.

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,20 +1,13 @@
 def encode(input_string):
 	encoded_data = ''
 	
-	input_string = input_string.decode('unicode-escape').encode('ascii')
-	print(input_string)
-	
 	input_length = len(input_string)
 	
 	current_char = input_string[0]
-	# print(current_char)
 	count = 1
 	for i in range(1, input_length):
-		# print(current_char, ' ', input_string[i])
-		
 		if (current_char == input_string[i]):
 			count += 1
-			# print(count)
 		else:
 			if (1 == count):
 				encoded_data += current_char
@@ -23,14 +16,47 @@ def encode(input_string):
 			
 			current_char = input_string[i]
 			count = 1
-		
+	
+	# Concat the last processed character and return the encoding.
 	if (1 == count):
 		encoded_data += current_char
 	else:
 		encoded_data += str(count) + current_char
-	# Concat the last processed character and return the encoding.
-	print(encoded_data)
+
 	return encoded_data
 
+def is_digit(character):
+	if ((47 < ord(character)) and (58 > ord(character))):
+		return True
+	
+	return False
+
 def decode(input_string):
-	return True
+	decoded_string = ''
+	
+	input_length = len(input_string)
+	
+	i = 0
+	while (i < input_length):
+		capturing_group = ''
+		count = 1
+		if (not is_digit(input_string[i])):
+			capturing_group += input_string[i]
+			character = capturing_group
+		else:
+			while (is_digit(input_string[i])):
+				capturing_group += input_string[i]
+				i += 1
+			
+			capturing_group += input_string[i]
+			
+			limit = len(capturing_group)
+			count = int(capturing_group[:limit - 1])
+			character = capturing_group[limit - 1]
+		
+		for j in range(0, count):
+			decoded_string += character
+		
+		i += 1
+	
+	return decoded_string
