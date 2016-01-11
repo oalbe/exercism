1fd4844b02790c4cecb4c1a2749c5563b3cbada7
Alberto <alberto.ognibene@gmail.com>
2016-01-11 18:08:42 +0100

    (python) series.py - Slightly refactored.

--- python/series/series.py
+++ python/series/series.py
@@ -1,12 +1,13 @@
 def slices(serie, slices_num):
-	if (slices_num > len(serie)) or (slices_num < 1):
+	if ((slices_num > len(serie)) or (slices_num < 1)):
 		raise ValueError('Slice size is too big.')
 	
-	limit = len(serie) - (slices_num - 1)
 	generated_slices = []
 	
+	limit = len(serie) - (slices_num - 1)
 	for i in range(limit):
 		current_slice = []
+		
 		for span in range(slices_num):
 			current_slice.append(int(serie[i + span]))
 		
