3a93e180561110669695c4849b70e794efe9064a
Alberto <alberto.ognibene@gmail.com>
2016-01-12 10:38:49 +0100

    (python) pangram.py - Removed the regex since it was an overkill.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,11 +1,12 @@
 import re
+import string
 
 def is_pangram(input_string):
 	if ('' == input_string):
 		return False
-	
-	clean_string = re.sub(r"[-!#$%^&*()_+|~=`{}\[\]:\";'<>?,.\/\s]*", "", input_string.lower())
-	
+
+	clean_string = input_string.lower()
+
 	for i in range(97, 123):
 		if (chr(i) not in clean_string):
 			return False
