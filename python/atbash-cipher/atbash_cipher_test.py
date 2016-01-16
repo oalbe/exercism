fcb74109a188821dd27fa4b3deb4526b2811183d
Alberto <alberto.ognibene@gmail.com>
2016-01-16 10:30:52 +0100

    (python) atbash-cipher.py - First seven tests passing.

--- python/atbash-cipher/atbash_cipher_test.py
+++ python/atbash-cipher/atbash_cipher_test.py
@@ -14,19 +14,19 @@ class AtbashCipherTest(unittest.TestCase):
     def test_encode_OMG(self):
         self.assertMultiLineEqual("lnt", encode("OMG"))
 
-    # def test_encode_O_M_G(self):
-    #     self.assertMultiLineEqual("lnt", encode("O M G"))
+    def test_encode_O_M_G(self):
+        self.assertMultiLineEqual("lnt", encode("O M G"))
 
-    # def test_encode_long_word(self):
-    #     self.assertMultiLineEqual("nrmwy oldrm tob", encode("mindblowingly"))
+    def test_encode_long_word(self):
+        self.assertMultiLineEqual("nrmwy oldrm tob", encode("mindblowingly"))
 
-    # def test_encode_numbers(self):
-    #     self.assertMultiLineEqual("gvhgr mt123 gvhgr mt",
-    #                               encode("Testing, 1 2 3, testing."))
+    def test_encode_numbers(self):
+        self.assertMultiLineEqual("gvhgr mt123 gvhgr mt",
+                                  encode("Testing, 1 2 3, testing."))
 
-    # def test_encode_sentence(self):
-    #     self.assertMultiLineEqual("gifgs rhurx grlm",
-    #                               encode("Truth is fiction."))
+    def test_encode_sentence(self):
+        self.assertMultiLineEqual("gifgs rhurx grlm",
+                                  encode("Truth is fiction."))
 
     # def test_encode_all_things(self):
     #     plaintext = "The quick brown fox jumps over the lazy dog."
