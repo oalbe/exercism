020f917b2ab9aa3d552f11570b1712d4a7d07c7e
Alberto <alberto.ognibene@gmail.com>
2016-01-16 10:29:33 +0100

    (python) atbash_cipher.py - First three tests passing.

---
+++ python/atbash-cipher/atbash_cipher.py
@@ -0,0 +1,23 @@
+import re
+
+def encode(plain_text):
+	clean_input = plain_text.lower()
+	regex = re.compile(r'[,\.\s+]')
+	clean_input = regex.sub('', clean_input)
+
+	encoded_text = '';
+	
+	input_len = len(clean_input)
+	for i in range(input_len):
+		if ('a' <= clean_input[i]) and ('z' >= clean_input[i]):
+			encoded_text += chr(122 - (ord(clean_input[i]) - 97))
+		elif ('0' <= clean_input[i] and '9' >= clean_input[i]):
+			encoded_text += clean_input[i]
+
+		if (4 == (i % 5)):
+			encoded_text += " "
+	
+	return encoded_text.rstrip();
+	
+def decode(encoded_text):
+	return 'a'
