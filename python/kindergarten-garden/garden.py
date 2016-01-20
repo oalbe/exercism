ab218fa8f56c552cea0d5783a09b5eb76c387fcb
Alberto <alberto.ognibene@gmail.com>
2016-01-20 11:31:07 +0100

    (python) garden.py - Done.

--- python/kindergarten-garden/garden.py
+++ python/kindergarten-garden/garden.py
@@ -1,19 +1,5 @@
 class Garden:
-	students = {
-		'Alice': 0,
-		'Bob': 2,
-		'Charlie': 4,
-		'David': 6,
-		'Eve': 8,
-		'Fred': 10,
-		'Ginny': 12,
-		'Harriet': 14,
-		'Ileana': 16,
-		'Joseph': 18,
-		'Kincaid': 20,
-		'Larry': 22
-	}
-	
+	students = {}
 	plants_varietys = {
 		'C': 'Clover',
 		'G': 'Grass',
@@ -21,8 +7,36 @@ class Garden:
 		'V': 'Violets'
 	}
 	
-	def __init__(self, plants_string):
+	def __init__(self, plants_string, students=None):
+		if (None == students):
+			self.students = {
+				'Alice': 0,
+				'Bob': 2,
+				'Charlie': 4,
+				'David': 6,
+				'Eve': 8,
+				'Fred': 10,
+				'Ginny': 12,
+				'Harriet': 14,
+				'Ileana': 16,
+				'Joseph': 18,
+				'Kincaid': 20,
+				'Larry': 22
+			}
+		else:
+			sorted_students = sorted(students)
+			self.students = self.parse_students(sorted_students)
+			
 		self.garden = plants_string.split('\n')
+	
+	def parse_students(self, students_list):
+		students_dict = {}
+
+		limit = len(students_list)
+		for i in range(limit):
+			students_dict[students_list[i]] = i * 2
+
+		return students_dict
 	
 	def plants(self, student):
 		student_plants = []
