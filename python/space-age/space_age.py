d4aa409dd1d6ce3dc583c910e2c4c7273c8d9405
Alberto <alberto.ognibene@gmail.com>
2016-01-22 12:58:08 +0100

    (python) space_age.py - Removed some code duplication with the aid of an helper method.

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,29 +1,43 @@
 import math
 
+convert_values = {
+	'earth' : 31557600,
+	'mercury' : 7600543.81992,
+	'venus' : 19414149.052176,
+	'mars': 59354032.69008,
+	'jupiter': 374355659.124,
+	'saturn': 929292362.8848,
+	'uranus': 2651370019.3296,
+	'neptune': 5200418560.032,
+}
+
 class SpaceAge:
 	def __init__(self, sec):
 		self.seconds = sec
 	
+	def convert_helper(self, planet):
+		return round(self.seconds / convert_values[planet], 2)
+	
 	def on_earth(self):
-		return round(self.seconds / 31557600, 2)
+		return self.convert_helper("earth")
 	
 	def on_mercury(self):
-		return round(self.seconds / 7600543.81992, 2)
+		return self.convert_helper("mercury")
 	
 	def on_venus(self):
-		return round(self.seconds / 19414149.052176, 2)
+		return self.convert_helper("venus")
 	
 	def on_mars(self):
-		return round(self.seconds / 59354032.69008, 2)
+		return self.convert_helper("mars")
 	
 	def on_jupiter(self):
-		return round(self.seconds / 374355659.124, 2)
+		return self.convert_helper("jupiter")
 	
 	def on_saturn(self):
-		return round(self.seconds / 929292362.8848, 2)
+		return self.convert_helper("saturn")
 	
 	def on_uranus(self):
-		return round(self.seconds / 2651370019.3296, 2)
+		return self.convert_helper("uranus")
 	
 	def on_neptune(self):
-		return round(self.seconds / 5200418560.032, 2)
+		return self.convert_helper("neptune")
