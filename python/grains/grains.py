4b6744f828e9d7c65dbd8b3ee76492763629e7cd
Alberto <alberto.ognibene@gmail.com>
2016-01-23 12:43:07 +0100

    (python) grains.py - Removed debug comments. Performance and readability look okay to me.

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,15 +1,11 @@
 def on_square(square):
-	# print('square = ', square)
-	# print('square - 1 = ', square - 1)
-	# print('2 ** (square - 1) = ', 2 ** (square - 1))
 	return 2 ** (square - 1)
 
 def total_after(square):
 	total_grains = 0
 	
-	for i in range(1, square + 1):
-		# print(total_grains)
-		# print("#", i, " = ", on_square(i))
+	square += 1
+	for i in range(1, square):
 		total_grains += on_square(i)
 	
 	return total_grains	
