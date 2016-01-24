5236d9dd38b66bb7c6a580ca60fa4119683cc4d5
Alberto <alberto.ognibene@gmail.com>
2016-01-24 12:41:49 +0100

    (python) robot_simulator.py - Refactored and removed some complication in advance() and simulate() methods.

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -4,8 +4,14 @@ class Robot:
 	directions = [NORTH, EAST, SOUTH, WEST]
 	
 	def __init__(self, direction=NORTH, x=0, y=0):
-		self.coordinates = (x, y)
+		self.x = x
+		self.y = y
 		self.bearing = direction
+	
+	@property
+	def coordinates(self):
+	    return (self.x, self.y)
+	
 	
 	def turn_helper(self, side):
 		discriminant = 1 if side else -1
@@ -21,21 +27,20 @@ class Robot:
 	
 	def advance(self):
 		if (NORTH == self.bearing):
-			self.coordinates = (self.coordinates[0], self.coordinates[1] + 1)
+			self.y += 1
 		elif (EAST == self.bearing):
-			self.coordinates = (self.coordinates[0] + 1, self.coordinates[1])
+			self.x += 1
 		elif (SOUTH == self.bearing):
-			self.coordinates = (self.coordinates[0], self.coordinates[1] - 1)
+			self.y -= 1
 		elif (WEST == self.bearing):
-			self.coordinates = (self.coordinates[0] - 1, self.coordinates[1])
+			self.x -= 1
+	
+	commands = {
+		'A': advance,
+		'L': turn_left,
+		'R': turn_right
+	}
 	
 	def simulate(self, commands_string):
-		commands_len = len(commands_string)
-		
-		for i in range(commands_len):
-			if ("L" == commands_string[i]):
-				self.turn_left()
-			elif ("R" == commands_string[i]):
-				self.turn_right()
-			elif("A" == commands_string[i]):
-				self.advance()
+		for instruction in commands_string:
+			self.commands[instruction](self)
