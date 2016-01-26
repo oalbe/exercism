7612e1052c0b1c7219ac6fcd644d311d7128776d
Alberto <alberto.ognibene@gmail.com>
2016-01-26 11:49:22 +0100

    (python) luhn.py - Done.

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,30 +1,43 @@
 class Luhn:
-	def __init__(self, num):
-		self._number = str(num)
-		self._addends = self.addends()
-	
-	def addends(self):
-		digits = self._number
-		addends_arr = []
-	
-		num_len = len(digits)
-		for i in range(num_len - 1, -1, -1):
-			if (0 == (num_len - i) % 2):
-				doubled_num = int(digits[i]) * 2
-				addends_arr.append((doubled_num - 9) if (doubled_num > 9) else doubled_num)
-			else:
-				addends_arr.append(int(digits[i]))
-		
-		return addends_arr
-	
-	def checksum(self):
-		computed_checksum = 0
-		
-		addends_len = len(self._addends)
-		for i in range(addends_len):
-			computed_checksum += self._addends[i]
-		
-		return computed_checksum
-	
-	def is_valid(self):
-		return True if (0 == (self.checksum() % 10)) else False
+    def __init__(self, num):
+        self._number = str(num)
+        self._addends = self.addends()
+    
+    def addends(self):
+        digits = self._number
+        addends_arr = []
+    
+        num_len = len(digits)
+        for i in range(num_len - 1, -1, -1):
+            if (0 == (num_len - i) % 2):
+                doubled_num = int(digits[i]) * 2
+                addends_arr.append((doubled_num - 9) if (doubled_num > 9) else doubled_num)
+            else:
+                addends_arr.append(int(digits[i]))
+        
+        return addends_arr
+    
+    def checksum(self):
+        computed_checksum = 0
+        
+        addends_len = len(self._addends)
+        for i in range(addends_len):
+            computed_checksum += self._addends[i]
+        
+        return computed_checksum
+    
+    def is_valid(self):
+        return True if (0 == (self.checksum() % 10)) else False
+    
+    # Theoretically, this is the way to create static methods. (without the `self`)
+    def create(num):
+        num = str(num)
+        padded_input = int(num + '0')
+        number = Luhn(padded_input)
+
+        if (not number.is_valid()):
+            num_checksum = str(number.checksum())
+            generated_check_digit = 10 - int(num_checksum[len(num_checksum) - 1])
+            return int(num + str(generated_check_digit))
+        
+        return padded_input
--- python/luhn/luhn_test.py
+++ python/luhn/luhn_test.py
@@ -15,7 +15,7 @@ class LuhnTests(unittest.TestCase):
         self.assertEqual(Counter([7, 6, 6, 1]),
                          Counter(Luhn(8631).addends()))
 
-    ## Probably there's a problem with this test.
+    # Probably there's a problem with this test.
     def test_checksum1(self):
         self.assertEqual(22, Luhn(4913).checksum())
 
@@ -28,22 +28,22 @@ class LuhnTests(unittest.TestCase):
     def test_valid_number(self):
         self.assertTrue(Luhn(8739567).is_valid())
 
-    # def test_create_valid_number1(self):
-    #     self.assertEqual(1230, Luhn.create(123))
+    def test_create_valid_number1(self):
+        self.assertEqual(1230, Luhn.create(123))
 
-    # def test_create_valid_number2(self):
-    #     self.assertEqual(8739567, Luhn.create(873956))
+    def test_create_valid_number2(self):
+        self.assertEqual(8739567, Luhn.create(873956))
 
-    # def test_create_valid_number3(self):
-    #     self.assertEqual(8372637564, Luhn.create(837263756))
+    def test_create_valid_number3(self):
+        self.assertEqual(8372637564, Luhn.create(837263756))
 
-    # def test_is_valid_can_be_called_repeatedly(self):
-    #     # This test was added, because we saw many implementations
-    #     # in which the first call to is_valid() worked, but the
-    #     # second call failed().
-    #     number = Luhn(8739567)
-    #     self.assertTrue(number.is_valid())
-    #     self.assertTrue(number.is_valid())
+    def test_is_valid_can_be_called_repeatedly(self):
+        # This test was added, because we saw many implementations
+        # in which the first call to is_valid() worked, but the
+        # second call failed().
+        number = Luhn(8739567)
+        self.assertTrue(number.is_valid())
+        self.assertTrue(number.is_valid())
 
 
 if __name__ == '__main__':
