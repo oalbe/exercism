750746af8bd808307cf963a7c4306cc29df2379c
Alberto <alberto.ognibene@gmail.com>
2016-01-29 12:02:48 +0100

    (python) prime_factors.py - Done.

---
+++ python/prime-factors/prime_factors.py
@@ -0,0 +1,14 @@
+def prime_factors(num):
+    factors = []
+    
+    i = 2
+    while (i <= num):
+        if 0 == (num % i):
+            factors.append(i)
+            
+            num = int(num / i)
+            i = 1
+        
+        i += 1
+    
+    return factors
