65acb3fec0993528a18eae0ac144005811f1f3f4
Alberto <alberto.ognibene@gmail.com>
2016-01-31 12:30:26 +0100

    (python) cipher.py - Refactored Caesan cipher and removed some duplication.

--- python/simple-cipher/cipher.py
+++ python/simple-cipher/cipher.py
@@ -2,31 +2,27 @@ import re
 
 
 class Caesar():
-	def encode(self, plain_text):
-		encoded_text = ''
+	def translate_helper(self, text, mode):
+		translated_text = ''
 		
+		offset = 3 if mode else -3
+		
+		text_len = len(text)
+		for i in range(text_len):
+			character = ((ord(text[i]) - 97) + offset) % 26
+			translated_text += chr(character + 97)
+		
+		return translated_text
+
+	def encode(self, plain_text):
 		clean_input = plain_text.lower()
 		regex = re.compile(r'[0-9!,\.\'\s+]')
 		clean_input = regex.sub('', clean_input)
-		
-		# plain_text = 'abcdefghijklmnopqrstuvwxyz'
-		clean_input_len = len(clean_input)
-		for i in range(clean_input_len):
-			character = ((ord(clean_input[i]) - 97) + 3) % 26
-			encoded_text += chr(character + 97)
-			# print(clean_input[i], ' = ', ((ord(clean_input[i]) - 97) + 3) %26)
-		
-		return encoded_text
+
+		return self.translate_helper(clean_input, True)
 	
 	def decode(self, encoded_text):
-		decoded_text = ''
-		
-		encoded_text_len = len(encoded_text)
-		for i in range(encoded_text_len):
-			character = ((ord(encoded_text[i]) - 97) - 3) % 26
-			decoded_text += chr(character + 97)
-		
-		return decoded_text
+		return self.translate_helper(encoded_text, False)
 
 class Cipher():
 	def encode(plain_text):
