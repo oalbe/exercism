de364abcbb110cb879c396a5ab997d6f37edfec2
Alberto <alberto.ognibene@gmail.com>
2016-02-19 17:01:27 +0100

    (python) handshake.py - First three tests for code() function done.

--- python/secret-handshake/handshake.py
+++ python/secret-handshake/handshake.py
@@ -8,32 +8,55 @@ commands = {
 def handshake(code_num):
     binary_code = code_num
     if type(code_num) is int:
+        if code_num < 0:
+            return []
+
         # `[2:]` cuts the `0b` at the beginning of the binary number
         binary_code = str(bin(code_num)[2:])
-    
-    print(binary_code)
-    
+
     secret = []
-    binary_code_len = len(binary_code)
     
     revert = False
-    if binary_code_len > 4:
+    if len(binary_code) > 4:
         binary_code = binary_code[1:]
         revert = True
-        binary_code_len = len(binary_code)
-    
+
+    binary_code_len = len(binary_code)
     for i in range(binary_code_len - 1, -1, -1):
-    # for i in range(0, binary_code_len):
+        if int(binary_code[i]) > 1:
+            return []
+
         command_index = int(binary_code[i]) * 10 ** (binary_code_len - i -1)
-        print("command_index = ", command_index)
         if 0 < command_index:
-            print("commands[command_index] = ", commands[command_index])
             secret.append(commands[command_index])
-    
+
     if revert:
-        return list(reversed(secret))
+        # reverse the list
+        return secret[::-1]
 
     return secret
 
+def extract_key(value):
+    for key, element in commands.items():
+        if element == value:
+            return key
+    
+    return False
+
 def code(code_list):
-    return 0
+    num = ''
+
+    # print(extract_key("wink"))
+    for element in code_list:
+        extracted_key = extract_key(element)
+
+        if not extracted_key:
+            return '0'
+        
+        if num:
+            num = str(extracted_key)[0] + str(num)
+        else:
+            num += str(extracted_key)
+        
+    
+    return num
--- python/secret-handshake/handshake_test.py
+++ python/secret-handshake/handshake_test.py
@@ -4,11 +4,11 @@ from handshake import handshake, code
 
 
 class HandshakeTest(unittest.TestCase):
-    def test_shake_int(self):
-        self.assertEqual(['wink', 'jump'], handshake(9))
+    # def test_shake_int(self):
+    #     self.assertEqual(['wink', 'jump'], handshake(9))
 
-    def test_shake_bin1(self):
-        self.assertEqual(['close your eyes', 'double blink'], handshake('10110'))
+    # def test_shake_bin1(self):
+    #     self.assertEqual(['close your eyes', 'double blink'], handshake('10110'))
 
     # def test_shake_bin2(self):
     #     self.assertEqual(['wink', 'close your eyes'], handshake('101'))
@@ -19,14 +19,14 @@ class HandshakeTest(unittest.TestCase):
     # def test_shake_bin_invalid(self):
     #     self.assertEqual([], handshake('121'))
 
-    # def test_unknown_action(self):
-    #     self.assertEqual('0', code(['wink', 'sneeze']))
+    def test_unknown_action(self):
+        self.assertEqual('0', code(['wink', 'sneeze']))
 
-    # def test_code1(self):
-    #     self.assertEqual('1100', code(['close your eyes', 'jump']))
+    def test_code1(self):
+        self.assertEqual('1100', code(['close your eyes', 'jump']))
 
-    # def test_code2(self):
-    #     self.assertEqual('11', code(['wink', 'double blink']))
+    def test_code2(self):
+        self.assertEqual('11', code(['wink', 'double blink']))
 
     # def test_code3(self):
     #     self.assertEqual('11010', code(['jump', 'double blink']))
