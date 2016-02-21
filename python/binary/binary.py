790af90d95fef55b95f4fb7b277a42fcea3e11cf
Alberto <alberto.ognibene@gmail.com>
2016-02-21 12:19:13 +0100

    (python) binary.py - Done.

--- python/binary/binary.py
+++ python/binary/binary.py
@@ -5,8 +5,19 @@ def validate_binary_string(binary_string):
 
     return True
 
+def to_dec(binary_string):
+    binary_string = binary_string[::-1]
+    decimal = 0
+    
+    binary_string_len = len(binary_string)
+    for i in range(binary_string_len):
+        decimal += int(binary_string[i]) * (2 ** i)
+
+    return decimal
+
 def parse_binary(binary_string):
     if not validate_binary_string(binary_string):
         raise ValueError("The string is not a valid binary number.")
 
-    return int(binary_string, 2)
+    # return int(binary_string, 2)
+    return to_dec(binary_string)
