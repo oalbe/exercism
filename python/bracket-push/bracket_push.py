27097a2b41b00604e4dcb2d4bd746e9180424ef7
Alberto <alberto.ognibene@gmail.com>
2016-02-23 17:17:01 +0100

    (python) bracket_push.py - Done.

---
+++ python/bracket-push/bracket_push.py
@@ -0,0 +1,33 @@
+def closing_bracket(br):
+    if ('}' == br) or (']' == br) or (')' == br):
+        return True
+    
+    return False
+
+def bracket_match(opening, closing):
+    if ('(' == opening) and (')' == closing):
+        return True
+    
+    if ('[' == opening) and (']' == closing):
+        return True
+    
+    if ('{' == opening) and ('}' == closing):
+        return True
+    
+    return False
+
+def pop_noexcept(stack):
+    return stack.pop() if not (len(stack) <= 0) else -1
+
+def check_brackets(br_input):
+    stack = []
+    
+    br_input_len = len(br_input)
+    for bracket in br_input:
+        if closing_bracket(bracket):
+            if not bracket_match(pop_noexcept(stack), bracket):
+                return False
+        else:
+            stack.append(bracket)
+
+    return (len(stack) <= 0)
