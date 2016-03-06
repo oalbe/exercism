c450a155d2bd0b5edd462fed3c5f7cc65c6e733a
Alberto <alberto.ognibene@gmail.com>
2016-03-06 19:19:26 +0100

    (python) queen_attack.py - Pythonize 'initialize_matrix()'.

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,67 +1,57 @@
-def initialize_matrix():
-    matrix = []
-    
-    for i in range(8):
-        matrix.append([])
-        for j in range(8):
-            matrix[i].append('_')
-
-    return matrix
+def init_matrix():
+    return [['_' for j in range(8)] for i in range(8)]
 
 def place_queens(white_queen, black_queen, board):
     board[white_queen[0]][white_queen[1]] = "W"
     board[black_queen[0]][black_queen[1]] = "B"
-    
+
     return board
 
 def valid_queen_position(queen):
     for coordinate in queen:
-        if coordinate < 0:
+        if coordinate < 0 or coordinate > 7:
             return False
-        
-        if coordinate > 7:
-            return False
-    
+
     return True
 
 def queen_overlap(white, black):
-    if white[0] == black[0] and white[1] == black[1]:
-        return True
-    
-    return False
-    
+    return white[0] == black[0] and white[1] == black[1]
+
+def valid_queens(white, black):
+    if queen_overlap(white, black):
+        return False
+
+    if valid_queen_position(white) != valid_queen_position(black):
+        return False
+
+    return True
+
 def board(white_queen, black_queen):
-    if queen_overlap(white_queen, black_queen):
-        raise ValueError("Queen positions can't overlap.")
+    if not valid_queens(white_queen, black_queen):
+        raise ValueError("Your queens got problems.")
 
-    if not (valid_queen_position(white_queen) and valid_queen_position(black_queen)):
-        raise ValueError("Queen position out of range.")
-
-    return [''.join(row) for row in place_queens(white_queen, black_queen, initialize_matrix())]
+    return [''.join(row) for row in place_queens(white_queen, black_queen, init_matrix())]
 
 def can_attack(white, black):
-    if queen_overlap(white, black):
-        raise ValueError("Queen positions can't overlap.")
-
-    if not (valid_queen_position(white) and valid_queen_position(black)):
-        raise ValueError("Queen position out of range.")
+    if not valid_queens(white, black):
+        raise ValueError("Your queens got problems.")
 
     # Check if they are in the same line
-    if (white[0] == black[0]):
+    if white[0] == black[0]:
         return True
-    
+
     # Check if they are in the same column
-    if (white[1] == black[1]):
+    if white[1] == black[1]:
         return True
-    
+
     # Check if they are in the same diagonal
     deltax = white[0] - black[0]
     deltay = white[1] - black[1]
 
-    if (deltay == -deltax):
+    if deltay == -deltax:
         return True
 
-    if (deltay == deltax):
+    if deltay == deltax:
         return True
-    
+
     return False
