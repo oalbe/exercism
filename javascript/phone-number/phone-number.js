7ff7adb55cdf38b499fdb9a60b64c5cf9194f128
Alberto <alberto.ognibene@gmail.com>
2016-03-10 21:53:16 +0100

    (javascript) phone-number.js - General refactoring.

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -4,32 +4,28 @@ var PhoneNumber = function(rawNumber) {
 
 PhoneNumber.prototype.number = function() {
     // match all opening and closing brackets, dashes, dots and whitespace
-    var regex = /[\(\)\-\.\s]/g;
-
     var cleanNumber = this._number;
-    if (regex.test(this._number)) {
-        cleanNumber = this._number.replace(regex, '');
+    
+    var regex = /[\(\)\-\.\s]/g;
+    if (regex.test(cleanNumber)) {
+        cleanNumber = cleanNumber.replace(regex, '');
     }
 
     var cleanNumLength = cleanNumber.length;
     if (10 === cleanNumLength) {
-        this._number = cleanNumber;
-    } else if ((11 === cleanNumLength) && ('1' === cleanNumber.charAt(0))) {
-        this._number = cleanNumber.substring(1, cleanNumLength);
-    } else { // number < 10, number > 11, number === 11 but not starting with '1'
-        this._number = '0000000000';
+        return cleanNumber;
+    } else if ((11 === cleanNumLength) && ('1' === cleanNumber[0])) {
+        return cleanNumber.substring(1, cleanNumLength);
     }
-
-    return this._number;
+    
+    // number < 10, number > 11, number === 11 but not starting with '1'
+    return '0000000000';
 };
 
 PhoneNumber.prototype.toString = function() {
     var localNumber = this.number();
-    return (
-        formattedNumber = '(' + localNumber.substr(0, 3) + ') ' + 
-            localNumber.substr(3, 3) + '-' + 
-            localNumber.substring(6, localNumber.length)
-    );
+    return '(' + localNumber.substr(0, 3) + ') ' + localNumber.substr(3, 3) + '-' + 
+           localNumber.substring(6);
 };
 
 PhoneNumber.prototype.areaCode = function () {
