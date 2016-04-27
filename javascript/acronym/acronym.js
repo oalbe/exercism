cba3b2c0610aa43a4b341280e38fccc60a77ba54
Alberto <alberto.ognibene@gmail.com>
2016-04-27 17:20:17 +0200

    (javascript) acronym.js

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -4,7 +4,8 @@ Acronym.prototype.parse = function(name) {
     var split_name = name.replace(/[,]/).split(/[- ]/);
     
     var acronym_output = "";
-    for (var i = 0; i < split_name.length; ++i) {
+    var split_name_len = split_name.length;
+    for (var i = 0; i < split_name_len; ++i) {
         if (split_name[i] == split_name[i].toUpperCase()) {
             acronym_output += split_name[i][0];
             continue;
@@ -12,7 +13,8 @@ Acronym.prototype.parse = function(name) {
 
         var word = split_name[i];
         acronym_output += word[0];
-        for (var j = 1; j < word.length; ++j) {
+        var word_len = word.length;
+        for (var j = 1; j < word_len; ++j) {
             if (word[j] == word[j].toUpperCase()) {
                 acronym_output += word[j];
             }
